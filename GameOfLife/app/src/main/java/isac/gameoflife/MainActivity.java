792c5530a4483e2049d9eb6d2602b4e403201c1b
Luca Longobardi <luca.longobardi.93@gmail.com>
2017-03-13 12:59:45 +0100

    added methods for broadcast connection and communicatio

--- GameOfLife/app/src/main/java/isac/gameoflife/MainActivity.java
+++ GameOfLife/app/src/main/java/isac/gameoflife/MainActivity.java
@@ -10,6 +10,15 @@ import android.view.MenuItem;
 import android.view.Surface;
 import android.view.WindowManager;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -22,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private static boolean firstTime=true;
     private GridView gridView;
     private boolean portrait = true;
+    private static final String EXCHANGE_NAME = "broadcast";
+    private Channel channel;
+    Connection connection;
 
 
     @Override
@@ -98,5 +110,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Utility and connection private methods
+     */
+
+    private void createFanout() throws java.io.IOException,java.util.concurrent.TimeoutException{
+         ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+    }
+
+    private void bindQueueToChannel()throws java.io.IOException {
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+    }
+
+    private void publishFanout(byte[] body) throws java.io.IOException{
+        channel.basicPublish(EXCHANGE_NAME, "", null, body);
+
+    }
+
+    private void closeFanout() throws java.io.IOException,java.util.concurrent.TimeoutException{
+        channel.close();
+        connection.close();
+
     }
 }
